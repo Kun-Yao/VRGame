bcb28571b34569edb5bcb0f45ded062524aa76db
a979797979797 <a979797979797@gmail.com>
2020-07-30 23:03:49 +0800

    check point

---
+++ Assets/script/Lap.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lap : MonoBehaviour
+{
+    public int point = 0;
+    public int lastPoint = 0;
+    public int lap = 0;
+}
